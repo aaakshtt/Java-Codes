590c90363aa4b8815491c35036f78fdae508ac67
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-06-04 19:27:38 +0530

    Print the sum of n naturral  numbers;

---
+++ sum_ofn_naturalnumber.java
@@ -0,0 +1,26 @@
+package com.akshat;
+//Java program to demonstrate
+//working of Arrays.toString()
+
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+class problems {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n= sc.nextInt();
+		int sum = 0;
+	for(int i = 0;i<=n;i++) {
+		sum = sum +i;
+		}
+	System.out.println(sum);
+		
+		
+	}
+}
+
+
+	
+
+
