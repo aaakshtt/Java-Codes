e850b38f36909f70d012363c7d2e648bfb1f77f4
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-06-06 23:43:30 +0530

    Create inverse semi triangle pattern

---
+++ inverse_semi_triangle.java
@@ -0,0 +1,31 @@
+package com.akshat;
+//Java program to demonstrate
+//working of Arrays.toString()
+
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+class problems {
+	public static void main(String[] args){
+		int n= 4;
+		//outerloop
+		for(int i=4;i<=n && i>0;i--) {
+			//inner loop
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		}
+	}
+	
+
+
+
+	
+
+
+// ****
+// ***
+// **
+// *
