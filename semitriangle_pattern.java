80af0cafef147481f0de10f3569d3e268825f9af
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-06-06 23:27:15 +0530

    Print semi triangle pattern in java

---
+++ semitriangle_pattern.java
@@ -0,0 +1,29 @@
+package com.akshat;
+//Java program to demonstrate
+//working of Arrays.toString()
+
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+class problems {
+	public static void main(String[] args){
+		int n= 4;
+		//outerlloop
+		for(int i=1;i<=n;i++) {
+			//inner loop
+			for(int j= 1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	}
+
+// *
+// **
+// ***
+// ****
+
+	
+
+
