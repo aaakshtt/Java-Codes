fdb6ca4d7555b8ce386c152126575a5fa6b6273b
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-06-10 20:36:42 +0530

    Check if number isprime or not prime

---
+++ primeornotprime.java
@@ -0,0 +1,26 @@
+package com.akshat;
+
+import java.util.Scanner;
+public class questions {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.print(isprime(n));
+	}
+	static boolean isprime(int n) {
+		if(n<=1){
+			return false;
+		}
+		int c= 2;
+		while(c*c<=n) {
+			if(n%c ==0) {
+				return false;
+			}
+			c++;
+		}
+		return c*c>=n;
+		
+	}
+
+}
+
