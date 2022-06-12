0716927776f9885bd06e7cb68c61b789ced48c46
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-06-12 19:38:24 +0530

    FInd the specific element in an array

---
+++ find_element_in_array.java
@@ -0,0 +1,20 @@
+package akshat2;
+
+public class class3 {
+	public static void main(String[] args) {
+		int[] arr = {13,45,6,789,67,345};
+		find( arr, 6);
+		
+	}
+		static void find(int[] arr, int tofind) {
+			int i=0;
+			int start = arr[i];
+			System.out.println(start)
+			;System.out.println(tofind);
+			for( i =0;i < arr.length;i++) {
+				if( tofind ==arr[i]) {
+					System.out.println("The value is found at index  " + i); 
+				}
+			}
+		}
+}
