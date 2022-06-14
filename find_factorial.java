cacb0267cd5b30d3da753858eeb9465bd6038bdd
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-06-14 23:20:05 +0530

    Find the factorial of a number

---
+++ find_factorial.java
@@ -0,0 +1,32 @@
+package akshat2;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+
+public class Solution {
+    public static void main(String[] args) {
+    	int number=  7;
+    	int fact =1,i;
+    	for(i = 1;i<number;i++) {
+    		fact = fact*i;
+    	}
+    	System.out.println(fact);
+    
+    	
+    }
+   
+    	
+    
+}
