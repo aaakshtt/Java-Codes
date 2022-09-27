31d7ebad55ba757edf05870e55439c6c09b25ddf
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-09-27 09:36:54 +0530

    Create print5natural.java

---
+++ print5natural.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static void main(String args[]) {
+        int num = 1;
+        while(num<=5){
+            System.out.println(num);
+            num +=1;
+        }
+    }
+
+}
+
+//Print 5 consecutive natural numebrs
