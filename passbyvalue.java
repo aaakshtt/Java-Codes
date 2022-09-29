b8db43be7882787bc920aab2181d9bf3ab0fd033
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-09-29 18:53:14 +0530

    Create passbyvalue.java

---
+++ passbyvalue.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static void main(String[] args) {
+        int x= 1;
+        increment(x);
+        System.out.println(x);
+
+    }
+    public static void increment(int x){
+        x++;
+        System.out.println(x);
+
+    }
+
+}
