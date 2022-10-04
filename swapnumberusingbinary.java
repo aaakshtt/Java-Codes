152fa1d969dd2e8e7ee0d5e693da4f85033320d4
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-10-04 10:46:44 +0530

    Create swapnumberusingbinary.java

---
+++ swapnumberusingbinary.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class GFG {
+    public static void main(String[] args) {
+      int a=39;
+      int b=87;
+      a= a^b;
+      b=a^b;
+      a=a^b;
+        System.out.println(a);
+    }
+}
+
+/*
+We swap the numbers by converting the and shifting to the 
