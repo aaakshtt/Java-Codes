0260a7f8ca0dd37741f4b0fb3b9084bc26474fa0
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-10-16 19:12:38 +0530

    Create increasingorderarray.java
    Check if an array is an increasing order with the help of recurssion in java

---
+++ increasingorderarray.java
@@ -0,0 +1,45 @@
+package akshat2;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+	
+	static boolean issorted(int arr[] , int index) {
+		
+		if(index ==  arr.length-1) {			// -1 is very important
+			System.out.println("true");
+			return true;						// base case 
+		}	
+		
+		if(arr[index] < arr[index+1]) {
+			//array is sorted
+			return issorted(arr,index+1);
+		}else{
+			System.out.println("false");
+			return false;
+		}
+		
+			
+	}
+    public static void main(String[] args) {
+    	int arr[] = {1,2,3,4};
+    	issorted(arr,0);
+    }
+}
+    
+    	
+    
+
+    
+    
+
