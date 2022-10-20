2e3a81a361d5bdd706975018b3242cfebb2be584
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-10-20 18:44:19 +0530

    Take user input from an user to create an array.

---
+++ userinputarray.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.io.IOException;
+import java.lang.*;
+import java.util.*;
+
+
+class Solution
+{
+    // Function to find the peak element
+    // arr[]: input array
+    // n: size of array a[]
+    public static void main(String[] args) throws IOException {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of elements you want to store: ");
+//reading the number of elements from the that we want to enter
+        n=sc.nextInt();
+//creates an array in the memory of length 10
+        int[] array = new int[2];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0; i<n; i++)
+        {
+//reading array elements from the user
+            array[i]=sc.nextInt();
+        }
+        System.out.println("Array elements are: ");
+// accessing array elements using the for loop
+        for (int i=0; i<n; i++)
+        {
+            System.out.println(array[i]);
+
+    }
+        int sum = 0;
+        System.out.println("Value of n is "+n);
+        for(int a=0;a<=n-1;a++){
+            sum = sum + array[a];
+        }
+        System.out.println(sum);
+
+
+
+}}
+
+
+
+
+
+
+
+
